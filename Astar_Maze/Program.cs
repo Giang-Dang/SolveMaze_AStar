1e29cd01070863f15f33c695b64a2eaba7239d6d
Đặng Vũ Ngọc Giang <89697095+Giang-Dang@users.noreply.github.com>
2022-07-23 17:47:25 +0700

    Add files via upload

--- Astar_Maze/Program.cs
+++ Astar_Maze/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System.Drawing;
 using AStar_Maze;
+using System.Diagnostics;
+using System.Threading;
 
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
 string filename = string.Empty;
 if (args[0] != null)
 {
     filename = args[0];
 }
 // try
-// {   
+// {  
     string path = $"{System.IO.Directory.GetCurrentDirectory()}\\{filename}";
     Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
     Bitmap Image = new Bitmap(path, true);
@@ -19,9 +23,11 @@ if (args[0] != null)
     Console.WriteLine("End : " + End.myPos.X + " " + End.myPos.Y);
     Console.WriteLine("==============");
     Console.ReadKey();
+    Console.WriteLine("Calculating...");
     List<Position> Result = new List<Position>();
     if (AStar.SolveMaze(ref maze, ref Start, ref End, ref Result))
     {
         Bitmap resImage =  ImageProcessing.EditImage(Image, Result, Color.Blue);
-        resImage.Save($"{System.IO.Directory.GetCurrentDirectory()}\\SolvedMaze.png");
+        stopwatch.Stop();
+        resImage.Save($"{System.IO.Directory.GetCurrentDirectory()}\\{filename}_result_{stopwatch.ElapsedMilliseconds}.png");
     }
